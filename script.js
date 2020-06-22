d14c3ae6edcab478f320a52cbe0290b219104666
Florencia <f.c.delabarra@gmail.com>
2020-06-22 15:50:19 -0400

    modified UI

--- script.js
+++ script.js
@@ -6,55 +6,61 @@ function computerPlay() {
 }
 
 
+let playerScore=0;
+let computerScore=0;
+
 function playRound(playerSelection, computerSelection) {
-    let playerS=playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1).toLowerCase();
-    let computerS=computerSelection.charAt(0).toUpperCase()+computerSelection.slice(1).toLowerCase();
-   
-    if (playerS=="Rock"&&computerS=="Scissors"||playerS=="Paper"&&computerS=="Rock"||playerS=="Scissors"&&computerS=="Paper"){
-     alert("You WIN!!! "+playerS+" beats "+computerS);
+ if (playerSelection=="Rock"&&computerSelection=="Scissors"||playerSelection=="Paper"&&computerSelection=="Rock"||playerSelection=="Scissors"&&computerSelection=="Paper"){
      playerScore++;
-     }
- else if (computerS=="Rock"&&playerS=="Scissors"||computerS=="Paper"&&playerS=="Rock"||computerS=="Scissors"&&playerS=="Paper"){
-     alert("You Lose :( "+computerS+" beats "+playerS);
-     computerScore++;
-     }
- else if (playerS==computerS){
-     alert("You are EVEN! "+playerS+" = "+computerS);
-     }
- else {
-     alert("ERROR!, try again");
-     }
-}
 
-
-
-let playerSelection;
-let computerSelection;
-let playerScore;
-let computerScore;
-
-
-
-function game(){
-    playerScore=0;
-    computerScore=0;
-    for(let i=1;i<=5;i++){
-        playerSelection = prompt("Play Round","Rock,Paper,Scissors");
-        computerSelection = computerPlay();
-        playRound(playerSelection, computerSelection);
-      }
-    if (playerScore<computerScore){
-        alert("Your Score is "+playerScore+"/5\nComputer's Score is "+computerScore+"/5\nYou Lose!");
-    } else if (playerScore>computerScore){
-        alert("Your Score is "+playerScore+"/5\nComputer's Score is "+computerScore+"/5\nYou Win!");
-    } else if(playerScore==computerScore){
-        alert("Your Score is "+playerScore+"/5\nComputer's Score is "+computerScore+"/5\nYou are Even!")
-    } else{
-        alert("Something went wrong!")
+     if(playerScore==5){
+      const content = document.createElement('li');
+      content.textContent = "You WIN!!! "+playerSelection+" beats "+computerSelection+", Your Score is "+playerScore+"/5, YOU ARE THE WINNER!";
+      container.appendChild(content);
+      playerScore=0;
+      computerScore=0;
+    }else{
+      const content = document.createElement('li');
+      content.textContent = "You WIN!!! "+playerSelection+" beats "+computerSelection+", Your Score is "+playerScore+"/5";
+      container.appendChild(content);
     }
 }
+else if (computerSelection=="Rock"&&playerSelection=="Scissors"||computerSelection=="Paper"&&playerSelection=="Rock"||computerSelection=="Scissors"&&playerSelection=="Paper"){
+     computerScore++;
 
-game();
+    if(computerScore==5){
+     const content = document.createElement('li');
+     content.textContent = "You Lose :( "+computerSelection+" beats "+playerSelection+", Computer Score is "+computerScore+"/5, COMPUTER IS THE WINNER!";
+     container.appendChild(content);
+     playerScore=0;
+     computerScore=0;
+    }else{
+     const content = document.createElement('li');
+     content.textContent = "You Lose :( "+computerSelection+" beats "+playerSelection+", Computer Score is "+computerScore+"/5";
+     container.appendChild(content);
+    }
+    }
+else if (playerSelection==computerSelection){
+     const content = document.createElement('li');
+     content.textContent = "You are EVEN! "+playerSelection+" = "+computerSelection;
+     container.appendChild(content);
+     }
+}
+
+
+document.getElementById("Rock").addEventListener("click", function() {
+  playRound("Rock",computerPlay());
+});
+document.getElementById("Paper").addEventListener("click", function() {
+  playRound("Paper",computerPlay());
+});
+document.getElementById("Scissors").addEventListener("click", function() {
+  playRound("Scissors",computerPlay());
+});
+
+
+const container = document.querySelector('#container');
+
 
 
 
