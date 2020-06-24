221ef79ae1e0d5df5d16585c4af2a2578b59bb82
Florencia <f.c.delabarra@gmail.com>
2020-06-24 12:05:57 -0400

    change styling

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
 function computerPlay() {
-    let results=["Rock","Paper","Scissors"];
-    let randomR= results[Math.floor(Math.random() * (3) )];
-    return randomR;
+  let results=["Rock","Paper","Scissors"];
+  let randomR= results[Math.floor(Math.random() * (3) )];
+  return randomR;
 }
 
 
@@ -10,52 +9,52 @@ let playerScore=0;
 let computerScore=0;
 
 function playRound(playerSelection, computerSelection) {
- if (playerSelection=="Rock"&&computerSelection=="Scissors"||playerSelection=="Paper"&&computerSelection=="Rock"||playerSelection=="Scissors"&&computerSelection=="Paper"){
-     playerScore++;
+if (playerSelection=="Rock"&&computerSelection=="Scissors"||playerSelection=="Paper"&&computerSelection=="Rock"||playerSelection=="Scissors"&&computerSelection=="Paper"){
+   playerScore++;
 
-     if(playerScore==5){
-      const content = document.createElement('li');
-      content.textContent = "You WIN!!! "+playerSelection+" beats "+computerSelection+", Your Score is "+playerScore+"/5, YOU ARE THE WINNER!";
-      container.appendChild(content);
-      playerScore=0;
-      computerScore=0;
-    }else{
-      const content = document.createElement('li');
-      content.textContent = "You WIN!!! "+playerSelection+" beats "+computerSelection+", Your Score is "+playerScore+"/5";
-      container.appendChild(content);
-    }
+   if(playerScore==5){
+    const content = document.createElement('div');
+    content.textContent = "You WIN!!! "+playerSelection+" beats "+computerSelection+", Your Score is "+playerScore+"/5, YOU ARE THE WINNER!";
+    container.appendChild(content);
+    playerScore=0;
+    computerScore=0;
+  }else{
+    const content = document.createElement('div');
+    content.textContent = "You WIN!!! "+playerSelection+" beats "+computerSelection+", Your Score is "+playerScore+"/5";
+    container.appendChild(content);
+  }
 }
 else if (computerSelection=="Rock"&&playerSelection=="Scissors"||computerSelection=="Paper"&&playerSelection=="Rock"||computerSelection=="Scissors"&&playerSelection=="Paper"){
-     computerScore++;
+   computerScore++;
 
-    if(computerScore==5){
-     const content = document.createElement('li');
-     content.textContent = "You Lose :( "+computerSelection+" beats "+playerSelection+", Computer Score is "+computerScore+"/5, COMPUTER IS THE WINNER!";
-     container.appendChild(content);
-     playerScore=0;
-     computerScore=0;
-    }else{
-     const content = document.createElement('li');
-     content.textContent = "You Lose :( "+computerSelection+" beats "+playerSelection+", Computer Score is "+computerScore+"/5";
-     container.appendChild(content);
-    }
-    }
+  if(computerScore==5){
+   const content = document.createElement('div');
+   content.textContent = "You Lose :( "+computerSelection+" beats "+playerSelection+", Computer Score is "+computerScore+"/5, COMPUTER IS THE WINNER!";
+   container.appendChild(content);
+   playerScore=0;
+   computerScore=0;
+  }else{
+   const content = document.createElement('div');
+   content.textContent = "You Lose :( "+computerSelection+" beats "+playerSelection+", Computer Score is "+computerScore+"/5";
+   container.appendChild(content);
+  }
+  }
 else if (playerSelection==computerSelection){
-     const content = document.createElement('li');
-     content.textContent = "You are EVEN! "+playerSelection+" = "+computerSelection;
-     container.appendChild(content);
-     }
+   const content = document.createElement('div');
+   content.textContent = "You are EVEN! "+playerSelection+" = "+computerSelection;
+   container.appendChild(content);
+   }
 }
 
 
 document.getElementById("Rock").addEventListener("click", function() {
-  playRound("Rock",computerPlay());
+playRound("Rock",computerPlay());
 });
 document.getElementById("Paper").addEventListener("click", function() {
-  playRound("Paper",computerPlay());
+playRound("Paper",computerPlay());
 });
 document.getElementById("Scissors").addEventListener("click", function() {
-  playRound("Scissors",computerPlay());
+playRound("Scissors",computerPlay());
 });
 
 
